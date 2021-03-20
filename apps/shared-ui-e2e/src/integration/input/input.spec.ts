c2d16b08bd96ff7a5793d7fd31cc82c0ce1b7dbb
Iorran <iorran.castro@vakt.io>
2021-03-20 16:43:31 +0000

    (fix) background input color test

--- apps/shared-ui-e2e/src/integration/input/input.spec.ts
+++ apps/shared-ui-e2e/src/integration/input/input.spec.ts
@@ -6,8 +6,7 @@ describe('Input component', () => {
   it('should have back background-color', () => {
     cy.findByRole('textbox')
       .should('have.css', 'background-color')
-      .and('eq', 'rgb(63, 60, 60)')
-      .should('have.css', 'background-color');
+      .and('eq', 'rgb(63, 60, 60)');
   }); 
   it('should have back color', () => {
     cy.findByRole('textbox') 
