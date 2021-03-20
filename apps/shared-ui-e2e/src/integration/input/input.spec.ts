acf0dfb0171d5ae4356c14c25f8e2c666b491861
Iorran <iorran.castro@vakt.io>
2021-03-20 16:38:25 +0000

    (test) check color input

--- apps/shared-ui-e2e/src/integration/input/input.spec.ts
+++ apps/shared-ui-e2e/src/integration/input/input.spec.ts
@@ -3,10 +3,15 @@ describe('Input component', () => {
   beforeEach(() => {
     cy.visit('/iframe.html?id=input--primary');
   })
- 
-  it('should have red border', () => {
+  it('should have back background-color', () => {
     cy.findByRole('textbox')
       .should('have.css', 'background-color')
-      .and('eq', 'rgb(63, 60, 60)');
+      .and('eq', 'rgb(63, 60, 60)')
+      .should('have.css', 'background-color');
+  }); 
+  it('should have back color', () => {
+    cy.findByRole('textbox') 
+      .should('have.css', 'color')
+      .and('eq', 'rgb(255, 255, 255)');
   }); 
 });
